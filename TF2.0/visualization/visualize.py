5cf2dda4951f04c77420d578d48a21a849df16ca
Tengyu Liu <24.jason.5@gmail.com>
2019-10-21 16:51:00 -0700

    update visualization

--- TF2.0/visualization/visualize.py
+++ TF2.0/visualization/visualize.py
@@ -78,7 +78,7 @@ def visualize(cup_id, cup_r, hand_z, offset=0):
         except:
             continue
 
-def visualize_hand(weights, rows, i):
+def visualize_hand(fig, weights, rows, i):
     xpos, xquat = ForwardKinematic(np.zeros([53]))
 
     obj_base = '../../data/hand'
@@ -86,6 +86,7 @@ def visualize_hand(weights, rows, i):
 
     start = 0
     end = 0
+
     for pid in range(4, 25):
         p = copy.deepcopy(stl_dict[parts[pid - 4]])
         try:
@@ -94,12 +95,12 @@ def visualize_hand(weights, rows, i):
             p.apply_transform(tm.transformations.quaternion_matrix(xquat[pid,:]))
             p.apply_translation(xpos[pid,:])
 
-            ax = plt.subplot(rows, 2, i * 2 - 1)
+            ax = fig.add_subplot(rows, 2, i * 2 - 1)
             pts = p.vertices[:,2] > 0.001
             ax.scatter(p.vertices[pts,0], p.vertices[pts,1], c=weights[start:end, 0][pts])
             ax.axis('off')
 
-            ax = plt.subplot(rows, 2, i * 2)
+            ax = fig.add_subplot(rows, 2, i * 2)
             pts = p.vertices[:,2] <= 0.001
             ax.scatter(p.vertices[pts,0], p.vertices[pts,1], c=weights[start:end, 0][pts])
             ax.axis('off')
@@ -110,43 +111,55 @@ def visualize_hand(weights, rows, i):
 
 data = pickle.load(open(os.path.join(os.path.dirname(__file__), '../figs', name, '%04d-%d.pkl'%(epoch, batch)), 'rb'))
 
-cup_id = np.array(data['cup_id'])
+cup_id = data['cup_id']
 cup_r = np.array(data['cup_r'])
 obs_z = np.array(data['obs_z'])
-obs_e = np.array(data['obs_e'])
+obs_e = data['obs_e']
 obs_w = np.array(data['obs_w'])
 syn_e = np.array(data['syn_e'])
 syn_z = np.array(data['syn_z'])
 syn_w = np.array(data['syn_w'])
 
-for i in range(11):
-    print(syn_z[:,0,i])
-    plt.plot(syn_z[:,0,i])
-plt.show()
+print('cup_r', cup_r.shape)
+print('obs_z', obs_z.shape)
+print('obs_w', obs_w.shape)
+print('syn_e', syn_e.shape)
+print('syn_z', syn_z.shape)
+print('syn_w', syn_w.shape)
 
-# plt.ion()
-# for i in range(91):
-#     print(i)
-#     plt.clf()
-#     visualize_hand(obs_w[0], 2, 1)
-#     visualize_hand(syn_w[i, 0], 2, 2)
-#     plt.pause(1e-5)
+fig = plt.figure(figsize=(6.40, 4.80), dpi=100)
+mlab.figure(size=(640,530))
 
-# cup_id = 1
-# cup_r = [[[1,0,0],[0,1,0],[0,0,1]]]
-# syn_z = np.random.normal(size=([11]))
-# syn_z[-3:] = [0, 0.2, 0.2]
-# syn_z[-9:-3] = 0
-# syn_z[-9] = 1
-# syn_z[-6] = 1
-# visualize(cup_id, cup_r[0], syn_z)
-# mlab.show()
+for i_batch in range(len(cup_r)):
+    for i_seq in range(len(syn_z)):
+        # Draw 3D grasping
+        mlab.clf()
+        visualize(cup_id, cup_r[i_batch], syn_z[i_seq][i_batch])
+        mlab.savefig('../figs/%s-%04d-%d-%d-%d-%d.png'%(name, epoch, batch, i_batch, i_seq, 1))
+        # Draw feature selection map
+        fig.clf()
+        visualize_hand(fig, obs_w[i_batch], 2, 1)
+        visualize_hand(fig, syn_w[i_seq, i_batch], 2, 2)
+        ax = fig.add_subplot(221)
+        ax.set_title('obs back')
+        ax = fig.add_subplot(222)
+        ax.set_title('obs front')
+        ax = fig.add_subplot(223)
+        ax.set_title('syn back')
+        ax = fig.add_subplot(224)
+        ax.set_title('syn front')
 
-# for i_batch in range(len(cup_r)):
-#     for i_seq in range(len(syn_z_seq)):
-#         mlab.clf()
-#         visualize(cup_id, cup_r[i_batch], syn_z_seq[i_seq][i_batch])
-#         mlab.savefig('../figs/%s-%04d-%d-%d-%d.png'%(name, epoch, batch, i_batch, i_seq))
-#     os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%%d.png ../figs/%s-%04d-%d-%d.gif'%(name, epoch, batch, i_batch, name, epoch, batch, i_batch))
-#     for i_seq in range(len(syn_z_seq)):
-#         os.remove('../figs/%s-%04d-%d-%d-%d.png'%(name, epoch, batch, i_batch, i_seq))
+        fig.savefig('../figs/%s-%04d-%d-%d-%d-%d.png'%(name, epoch, batch, i_batch, i_seq, 2))
+        # Merge two
+        os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%d-%d.png -i ../figs/%s-%04d-%d-%d-%d-%d.png -filter_complex hstack ../figs/%s-%04d-%d-%d-%d.png'%(
+            name, epoch, batch, i_batch, i_seq, 1, name, epoch, batch, i_batch, i_seq, 2, name, epoch, batch, i_batch, i_seq
+        ))
+        os.remove('../figs/%s-%04d-%d-%d-%d-1.png'%(name, epoch, batch, i_batch, i_seq))
+        os.remove('../figs/%s-%04d-%d-%d-%d-2.png'%(name, epoch, batch, i_batch, i_seq))
+
+    os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%%d.png -filter_complex "[0:v] palettegen" palette.png'%(name, epoch, batch, i_batch))
+    os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%%d.png -i palette.png -filter_complex "[0:v][1:v] paletteuse" ../figs/%s-%04d-%d-%d.gif'%(name, epoch, batch, i_batch, name, epoch, batch, i_batch))
+    os.remove('palette.png')
+    
+    for i_seq in range(len(syn_z)):
+        os.remove('../figs/%s-%04d-%d-%d-%d.png'%(name, epoch, batch, i_batch, i_seq))
