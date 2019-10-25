now sample keypoints uniformly from surface fixed issue with imbalanced gradient distribution fixed issue with imbalanced z distribution fixed issue with unregistered EMA averaging

TF2.0/HandModel.py
@@ -23,8 +23,9 @@ class HandModel:
                     'ring0', 'ring1', 'ring2', 'ring3',
                     'pinky0', 'pinky1', 'pinky2', 'pinky3']
 
-        self.surface_pts = {p: tf.constant(tm.load(os.path.join(os.path.dirname(__file__), '../data', 'hand', p + '.STL')).vertices, dtype=tf.float32) for p in self.parts}
+        # self.surface_pts = {p: tf.constant(tm.load(os.path.join(os.path.dirname(__file__), '../data', 'hand', p + '.STL')).vertices, dtype=tf.float32) for p in self.parts}
         # self.surface_pts = {p: tf.constant(np.mean(np.load(os.path.join(os.path.dirname(__file__), '../data', p + '.faces.npy')), axis=1), dtype=tf.float32) for p in self.parts if '0' not in p}
+        self.surface_pts = {p: tf.constant(np.load(os.path.join(os.path.dirname(__file__), '../data', p + '.surface_sample.npy')), dtype=tf.float32) for p in self.parts if '0' not in p}
         self.n_surf_pts = sum(x.shape[0] for x in self.surface_pts.values())
 
         # Input placeholder

TF2.0/model.py
@@ -54,7 +54,7 @@ class Model:
         pass
 
     def build_model(self):
-        self.EMA = tf.train.ExponentialMovingAverage(decay=0.9999)
+        self.EMA = tf.train.ExponentialMovingAverage(decay=0.99999)
         self.cup_models = { 
                             1: CupModel(1, self.cup_restore, self.cup_model_path), 
                             2: CupModel(2, self.cup_restore, self.cup_model_path), 
@@ -70,7 +70,7 @@ class Model:
         self.hand_model = HandModel(self.batch_size)
         self.touch_filter = TouchFilter(self.hand_model.n_surf_pts, situation_invariant=self.situation_invariant, penalty_strength=self.penalty_strength)
         print('Hand Model #PTS: %d'%self.hand_model.n_surf_pts)        
-
+        
         self.obs_ew = {i: self.descriptor(self.obs_z, self.cup_r, self.cup_models[i], reuse=(i!=1)) for i in range(1,self.cup_num + 1)}
         self.langevin_dynamics = {i : self.langevin_dynamics_fn(i) for i in range(1,self.cup_num + 1)}
         self.inp_ew = {i: self.descriptor(self.inp_z, self.cup_r, self.cup_models[i], reuse=True) for i in range(1,self.cup_num + 1)}
@@ -100,17 +100,23 @@ class Model:
 
     def langevin_dynamics_fn(self, cup_id):
         def langevin_dynamics(z, r):
+            z_weight = tf.constant([10, 10, 2, 2, 2, 2, 2, 2, 1, 1, 1], dtype=tf.float32)
             energy, weight = self.descriptor(z,r,self.cup_models[cup_id],reuse=True) #+ tf.reduce_mean(z[:,:self.hand_z_size] * z[:,:self.hand_z_size]) + tf.reduce_mean(z[:,self.hand_z_size:] * z[:,self.hand_z_size:])
+            energy = energy + tf.reduce_mean(tf.norm(z / z_weight, axis=-1)) * 3
             grad_z = tf.gradients(energy, z)[0]
             gz_abs = tf.abs(grad_z)
-            self.EMA.apply([gz_abs])
-
             if self.adaptive_langevin:
-                g_avg = self.EMA.average(gz_abs) + 1e-6
-                grad_z = grad_z / g_avg
+                apply_op = self.EMA.apply([gz_abs])
+                with tf.control_dependencies([apply_op]):
+                    g_avg = self.EMA.average(gz_abs) + 1e-6
+                    grad_z = grad_z / g_avg
             if self.clip_norm_langevin:
                 grad_z = tf.clip_by_norm(grad_z, 1)
-            z = z - self.step_size * grad_z + tf.random.normal(z.shape, mean=0.0, stddev=self.stddev)
+
+            grad_z = grad_z * z_weight
+            # p = tf.print('GRAD: ', grad_z, summarize=-1)
+            with tf.control_dependencies([p]):
+                z = z - self.step_size * grad_z # + tf.random.normal(z.shape, mean=0.0, stddev=self.stddev)
             return [z, energy, weight]
             
         return langevin_dynamics

TF2.0/train.py
@@ -27,7 +27,6 @@ print('PCA loaded.')
 cup_id_list = [1,2,3,4,5,6,7,8]
 if flags.debug:
     cup_id_list = [1,2]
-    flags.langevin_steps = 2
 
 cup_models = {cup_id: tm.load_mesh(os.path.join(project_root, 'data/cups/onepiece/%d.obj'%cup_id)) for cup_id in cup_id_list}
 print('Cup models loaded.')
@@ -46,6 +45,8 @@ for i in cup_id_list:
         for start_end in annotation:
             start, end = [int(x) for x in start_end.split(':')]
             for frame in range(start, end):
+                if flags.debug and len(cup_rs[i]) > 1:
+                    break
                 cup_id = i
                 cup_rotation = Q(mat_data[frame, 1 + 28 * 3 + 27 * 4: 1 + 28 * 3 + 28 * 4]).rotation_matrix
                 cup_translation = mat_data[frame, 1 + 27 * 3 : 1 + 28 * 3]
@@ -96,9 +97,6 @@ if flags.restore_batch >= 0 and flags.restore_epoch >= 0:
 
 print('Start training...')
 
-if flags.adaptive_langevin:
-    gradients = np.ones([10000, flags.z_size + 9])
-
 # train
 for epoch in range(flags.epochs):
     if epoch < flags.restore_epoch:
@@ -133,6 +131,7 @@ for epoch in range(flags.epochs):
             if langevin_step < 30: 
                 stddev = (30 - langevin_step) / 300
             syn_z, syn_e, syn_w = sess.run(model.syn_zew[cup_id], feed_dict={model.cup_r: cup_r, model.inp_z: syn_z, model.stddev: stddev})
+            # print(langevin_step, syn_z, syn_e, np.any(np.isnan(syn_w)), np.any(np.isinf(syn_w)))
             syn_z_seq.append(syn_z)
             syn_e_seq.append(syn_e)
             syn_w_seq.append(syn_w)
@@ -167,3 +166,5 @@ for epoch in range(flags.epochs):
             saver.save(sess, os.path.join(model_dir, '%04d-%d.ckpt'%(epoch, batch_id)))
 
     print()
+
+# python train.py --pca_size 44 --z_size 2 --use_pca --debug --batch_size 1 --step_size 0.01 --adaptive_langevin --langevin_steps 90 --clip_norm_langevin

TF2.0/visualization/visualize.py
@@ -13,6 +13,7 @@ import mat_trans as mt
 from forward_kinematics import ForwardKinematic
 
 import matplotlib.pyplot as plt
+from pyquaternion.quaternion import Quaternion as Q
 
 name = sys.argv[1]
 epoch = int(sys.argv[2])
@@ -81,32 +82,35 @@ def visualize(cup_id, cup_r, hand_z, offset=0):
 def visualize_hand(fig, weights, rows, i):
     xpos, xquat = ForwardKinematic(np.zeros([53]))
 
-    obj_base = '../../data/hand'
-    stl_dict = {obj: tm.load_mesh(os.path.join(obj_base, '%s.STL'%obj)) for obj in parts}
+    obj_base = '../../data'
+    stl_dict = {obj: np.load(os.path.join(obj_base, '%s.surface_sample.npy'%obj)) for obj in parts}
 
     start = 0
     end = 0
 
     for pid in range(4, 25):
+        if '0' in parts[pid - 4]:
+            continue
         p = copy.deepcopy(stl_dict[parts[pid - 4]])
         try:
-            end += len(p.vertices)
+            end += len(p)
 
-            p.apply_transform(tm.transformations.quaternion_matrix(xquat[pid,:]))
-            p.apply_translation(xpos[pid,:])
+            p = np.matmul(Q().rotation_matrix, p.T).T
+            p += xpos[[pid], :]
 
             ax = fig.add_subplot(rows, 2, i * 2 - 1)
-            pts = p.vertices[:,2] > 0.001
-            ax.scatter(p.vertices[pts,0], p.vertices[pts,1], c=weights[start:end, 0][pts])
+            pts = p[:,2] > 0.001
+            ax.scatter(p[pts,0], p[pts,1], c=weights[start:end, 0][pts])
             ax.axis('off')
 
             ax = fig.add_subplot(rows, 2, i * 2)
-            pts = p.vertices[:,2] <= 0.001
-            ax.scatter(p.vertices[pts,0], p.vertices[pts,1], c=weights[start:end, 0][pts])
+            pts = p[:,2] <= 0.001
+            ax.scatter(p[pts,0], p[pts,1], c=weights[start:end, 0][pts])
             ax.axis('off')
 
-            start += len(p.vertices)
+            start += len(p)
         except:
+            raise
             continue
 
 data = pickle.load(open(os.path.join(os.path.dirname(__file__), '../figs', name, '%04d-%d.pkl'%(epoch, batch)), 'rb'))
@@ -128,9 +132,13 @@ print('syn_z', syn_z.shape)
 print('syn_w', syn_w.shape)
 
 fig = plt.figure(figsize=(6.40, 4.80), dpi=100)
-mlab.figure(size=(640,530))
+mlab.figure(size=(640,523))
 
 for i_batch in range(len(cup_r)):
+    mlab.clf()
+    visualize(cup_id, cup_r[i_batch], obs_z[i_batch])
+    mlab.savefig('../figs/%s-%04d-%d-%d.png'%(name, epoch, batch, i_batch))
+
     for i_seq in range(len(syn_z)):
         # Draw 3D grasping
         mlab.clf()
@@ -156,14 +164,27 @@ for i_batch in range(len(cup_r)):
         fig.savefig('../figs/%s-%04d-%d-%d-%d-%d.png'%(name, epoch, batch, i_batch, i_seq, 2))
         # Merge two
         os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%d-%d.png -i ../figs/%s-%04d-%d-%d-%d-%d.png -filter_complex hstack ../figs/%s-%04d-%d-%d-%d.png'%(
-            name, epoch, batch, i_batch, i_seq, 1, name, epoch, batch, i_batch, i_seq, 2, name, epoch, batch, i_batch, i_seq
+            name, epoch, batch, i_batch, i_seq, 1, 
+            name, epoch, batch, i_batch, i_seq, 2, 
+            name, epoch, batch, i_batch, i_seq
         ))
+
+        os.system('ffmpeg -i ../figs/%s-%04d-%d-%d.png -i ../figs/%s-%04d-%d-%d-%d.png -filter_complex hstack ../figs/%s-%04d-%d-%d-%d.png'%(
+            name, epoch, batch, i_batch, 
+            name, epoch, batch, i_batch, i_seq,
+            name, epoch, batch, i_batch, i_seq
+        ))
+
+        exit()
+
         os.remove('../figs/%s-%04d-%d-%d-%d-1.png'%(name, epoch, batch, i_batch, i_seq))
         os.remove('../figs/%s-%04d-%d-%d-%d-2.png'%(name, epoch, batch, i_batch, i_seq))
 
-    os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%%d.png -filter_complex "[0:v] palettegen" palette.png'%(name, epoch, batch, i_batch))
-    os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%%d.png -i palette.png -filter_complex "[0:v][1:v] paletteuse" ../figs/%s-%04d-%d-%d.gif'%(name, epoch, batch, i_batch, name, epoch, batch, i_batch))
+    os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%%d-1.png -filter_complex "[0:v] palettegen" palette.png'%(name, epoch, batch, i_batch))
+    os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%%d-1.png -i palette.png -filter_complex "[0:v][1:v] paletteuse" ../figs/%s-%04d-%d-%d.gif'%(name, epoch, batch, i_batch, name, epoch, batch, i_batch))
     os.remove('palette.png')
-    
+
+    # os.system('ffmpeg -i ../figs/%s-%04d-%d-%d-%%d-1.png ../figs/%s-%04d-%d-%d.gif'%(name, epoch, batch, i_batch, name, epoch, batch, i_batch))
+
     for i_seq in range(len(syn_z)):
-        os.remove('../figs/%s-%04d-%d-%d-%d.png'%(name, epoch, batch, i_batch, i_seq))
+        os.remove('../figs/%s-%04d-%d-%d-%d-1.png'%(name, epoch, batch, i_batch, i_seq))