b25e9795b073434c3632014c23eb6c883be03724
Tengyu Liu <24.jason.5@gmail.com>
2019-11-17 10:49:32 -0800

    add train.sh

---
+++ data/cups/CupSDF/train.sh
@@ -0,0 +1,9 @@
+python train_cup.py --cup_id 1 --batch_size 1024
+python train_cup.py --cup_id 2 --batch_size 1024
+python train_cup.py --cup_id 3 --batch_size 1024
+python train_cup.py --cup_id 4 --batch_size 1024
+python train_cup.py --cup_id 5 --batch_size 1024
+python train_cup.py --cup_id 6 --batch_size 1024
+python train_cup.py --cup_id 7 --batch_size 1024
+python train_cup.py --cup_id 8 --batch_size 1024
+python train_cup.py --cup_id 9 --batch_size 1024
