5c47ebff2f2f868895435662d7439e3b7292cac6
Tengyu Liu <24.jason.5@gmail.com>
2019-11-18 15:36:44 -0800

    update SDF data

--- data/cups/CupSDF/prepare_data.py
+++ data/cups/CupSDF/prepare_data.py
@@ -6,50 +6,35 @@ import numpy as np
 for cup_id in range(1,11):
     cup = tm.load(os.path.join(os.path.dirname(__file__), '../onepiece/%d.obj'%cup_id))
     
-    pts = np.random.random((1000, 3)) * 0.2 - 0.1
-    pts = np.vstack([pts, obj.sample(1000)])
-    dists = tm.proximity.signed_distance(obj, pts)
+    pts = np.random.random((10000, 3)) * 0.2 - 0.1
+    pts = np.vstack([pts, cup.sample(10000)])
+    dists = tm.proximity.signed_distance(cup, pts)
 
     pts = pts[np.logical_not(np.isnan(dists)),:]
     dists = dists[np.logical_not(np.isnan(dists))]
 
-    xs = np.copy(pts)
-    ys = np.copy(dists)
+    step_size = 0.1
+    factor = 1
 
     step = 0
-    while sum(ys > 0) < 10000 or sum(ys < 0) < 10000:
+    while len(dists) < 500000:
         step += 1
         # print '%d'%step
         next_step = pts + np.random.random(pts.shape) * step_size - step_size / 2
-        new_distance = tm.proximity.signed_distance(obj, next_step)
+        new_distance = tm.proximity.signed_distance(cup, next_step)
         # print 'avg_sq_dist: ', np.mean(np.square(new_distance))
         acceptance = np.exp(np.minimum(np.power(dists, 2) - np.power(new_distance, 2), 0) * factor)
         discard = np.logical_or(acceptance < np.random.random(acceptance.shape), np.isnan(new_distance))
         next_step[discard] = pts[discard]
         new_distance[discard] = dists[discard]
         # print(discard.sum())
-        xs = np.vstack([xs, next_step[np.logical_not(discard), :]])
-        ys = np.concatenate([ys, new_distance[np.logical_not(discard)]])
-        pts = next_step
-        dists = new_distance
+        pts = np.vstack([pts, next_step[np.logical_not(discard), :]])
+        dists = np.concatenate([dists, new_distance[np.logical_not(discard)]])
 
-        sys.stdout.write('\r[%s] %d, %d'%(part, sum(ys > 0), sum(ys < 0)))
-        sys.stdout.flush()
+        print('\r[%d] %d, %d'%(cup_id, sum(dists > 0), sum(dists < 0)), end='', flush=True)
 
-    shuffle_idx = np.random.permutation(np.arange(len(ys)))
-    xs = xs[shuffle_idx,:]
-    ys = ys[shuffle_idx]
+    shuffle_idx = np.random.permutation(np.arange(len(dists)))
+    pts = pts[shuffle_idx,:]
+    dists = dists[shuffle_idx]
 
-    pos_x = xs[ys >= 0, :]
-    pos_y = ys[ys >= 0]
-
-    neg_x = xs[ys < 0, :]
-    neg_y = ys[ys < 0]
-
-    pos_x = pos_x[:10000]
-    neg_x = neg_x[:10000]
-    pos_y = pos_y[:10000]
-    neg_y = neg_y[:10000]
-
-    xs = np.vstack([pos_x, neg_x])
-    ys = np.concatenate([pos_y, neg_y])
+    np.save('cup_%d.npy'%cup_id, np.concatenate([pts.reshape([-1,3]), dists.reshape([-1,1])], axis=-1))
