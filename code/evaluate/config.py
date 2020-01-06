29dcd5db861537d137b7ae4bb39af904e3dd2264
Tengyu Liu <24.jason.5@gmail.com>
2020-01-06 15:23:37 -0800

    debug

--- code/evaluate/config.py
+++ code/evaluate/config.py
@@ -13,7 +13,7 @@ tf.flags.DEFINE_boolean('dynamic_z2', False, '')
 tf.flags.DEFINE_integer('z2_size', 10, '')
 tf.flags.DEFINE_float('random_scale', 0.3, 'for initializing syn position')
 tf.flags.DEFINE_float('prior_weight', 10, '')
-tf.flags.DEFINE_float('random_strength', 0.1, 'for langevin dynamics')
+tf.flags.DEFINE_float('random_strength', 0.0, 'for langevin dynamics')
 tf.flags.DEFINE_string('prior_type', 'NN', 'choose between "NN" and "Phys"')
 
 # Train
