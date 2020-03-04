7f447c8d0a3a9859582846965e46e3e816b02646
Tengyu Liu <24.jason.5@gmail.com>
2020-03-04 15:20:40 -0800

    debug

--- code/model.py
+++ code/model.py
@@ -89,7 +89,7 @@ class Model:
                             self.obs_z2_update[cup_id] = self.obs_z2[cup_id] - tf.gradients(self.obs_ewp[cup_id][0] + tf.reduce_mean(tf.norm(self.obs_z2[cup_id], axis=-1)), self.obs_z2[cup_id])[0] * self.d_lr
     
     def hand_prior_nn(self, hand_z):
-        with tf.variable_scope(tf.get_variable_scope()):
+        with tf.variable_scope('HandPriorNN', reuse=tf.AUTO_REUSE):
             tf.get_variable_scope().reuse_variables()
             h1 = fully_connected(hand_z, 64, activation_fn=tf.nn.relu, weight_decay=self.l2_reg, scope='hand_prior_1')
             h2 = fully_connected(h1, 64, activation_fn=tf.nn.relu, weight_decay=self.l2_reg, scope='hand_prior_2')
