d8c29993618bd99fc12b37e89897b7ef0f1b9fb7
Tengyu Liu <24.jason.5@gmail.com>
2020-03-04 15:39:01 -0800

    debug

--- code/utils/HandPriorNN.py
+++ code/utils/HandPriorNN.py
@@ -9,7 +9,7 @@ class HandPriorNN:
     
     def __call__(self, z):
         with tf.variable_scope('HandPriorNN', reuse=tf.AUTO_REUSE):
-            h1 = tf_util.fully_connected(hand_z, 64, activation_fn=tf.nn.relu, weight_decay=self.l2_reg, scope='hand_prior_1')
+            h1 = tf_util.fully_connected(z, 64, activation_fn=tf.nn.relu, weight_decay=self.l2_reg, scope='hand_prior_1')
             h2 = tf_util.fully_connected(h1, 64, activation_fn=tf.nn.relu, weight_decay=self.l2_reg, scope='hand_prior_2')
             prior = tf_util.fully_connected(h2, 1, activation_fn=None, weight_decay=self.l2_reg, scope='hand_prior_3')
             return prior
