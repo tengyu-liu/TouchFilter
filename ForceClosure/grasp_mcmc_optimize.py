84559c13a62485e68cbe23eb6e5518f831c668ef
Tengyu Liu <tengyuliu@ucla.edu>
2020-08-31 16:01:02 -0700

    optimize grasp_mcmc_optimize.py

--- ForceClosure/grasp_mcmc_optimize.py
+++ ForceClosure/grasp_mcmc_optimize.py
@@ -1,150 +1,89 @@
-import random
-import sys
+import argparse
+import os
 import pickle
+import random
+import shutil
+import sys
+import time
+from collections import defaultdict
 
-import plotly
-import plotly.graph_objects as go
+import numpy as np
+import tensorboard
+import torch
+import torch.nn as nn
+import torch.utils.tensorboard
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', default=16, type=int)
+parser.add_argument('--n_handcode', default=6, type=int)
+parser.add_argument('--n_contact', default=3, type=int)
+parser.add_argument('--mu', default=0.98, type=float)
+parser.add_argument('--name', default='exp', type=str)
+parser.add_argument('--id', default=0, type=int)
+args = parser.parse_args()
+
+np.seterr(all='raise')
+random.seed(args.id)
+np.random.seed(args.id)
+torch.manual_seed(args.id)
+args.name = args.name + '_%d'%args.id
 
 from CodeUtil import *
 from EMA import EMA
 from HandModel import HandModel
-from HCGraspPrediction import GraspPrediction
 from Losses import FCLoss
 from ObjectModel import ObjectModel
 from PenetrationModel import PenetrationModel
 
-name = sys.argv[1]
-
 hand_model = HandModel(flat_hand_mean=False)
 object_model = ObjectModel()
 fc_loss_model = FCLoss(object_model=object_model)
-grasp_prediction = GraspPrediction(num_cp=3, hand_code_length=hand_model.code_length, num_handpoint=hand_model.num_points).cuda()
 penetration_model = PenetrationModel(hand_model=hand_model, object_model=object_model)
 
-data = pickle.load(open('logs/%s/last.pkl'%(name), 'rb'))
+log_dir = os.path.join('logs', args.name)
+obj_code, z, contact_point_indices, energy, energy_history, temperature_history, stepsize_history = pickle.load(open(os.path.join(log_dir, 'saved_998000.pkl'), 'rb'))
 
-obj_code, z, contact_point_indices, energy, energy_history, temperature_history, stepsize_history = data
-
-# i_item = np.argmin(energy.detach().cpu().numpy())
-# i_item = int(random.random() * len(z))
-# i_item = 53
-# print(i_item)
-
-def compute_energy(obj_code, z, contact_point_indices, verbose=False, no_grad=False):
+def compute_energy(obj_code, z, contact_point_indices, verbose=False, sd_weight=1):
   hand_verts = hand_model.get_vertices(z)
-  contact_point = torch.stack([hand_verts[torch.arange(z.shape[0]), contact_point_indices[:,i],:] for i in range(3)], dim=1)
+  contact_point = torch.stack([hand_verts[torch.arange(batch_size), contact_point_indices[:,i],:] for i in range(args.n_contact)], dim=1)
   contact_distance = object_model.distance(obj_code, contact_point)
   contact_normal = object_model.gradient(contact_point, contact_distance)
+  contact_normal = contact_normal / torch.norm(contact_normal, dim=-1, keepdim=True)
+  hand_normal = hand_model.get_surface_normals(z=z)
+  hand_normal = torch.stack([hand_normal[torch.arange(z.shape[0]), contact_point_indices[:,i], :] for i in range(args.n_contact)], dim=1)
+  hand_normal = hand_normal / torch.norm(hand_normal, dim=-1, keepdim=True)    
+  normal_alignment = ((hand_normal * contact_normal).sum(-1) + 1).sum(-1)
+  linear_independence, force_closure = fc_loss.fc_loss(contact_point, contact_normal, obj_code)
+  surface_distance = fc_loss.dist_loss(obj_code, contact_point) * sd_weight
+  penetration = penetration_model.get_penetration_from_verts(obj_code, hand_verts)  # B x V
+  z_norm = torch.norm(z[:,-6:], dim=-1) * 0.1
+  if verbose:
+    return linear_independence, force_closure, surface_distance.sum(1), penetration.sum(1), z_norm, normal_alignment
+  else:
+    return linear_independence + force_closure + surface_distance.sum(1) + penetration.sum(1) + z_norm + normal_alignment
 
-  grad_op = torch.enable_grad
-  if no_grad:
-    grad_op = torch.no_grad
+mask = torch.tensor(np.eye(15)).float().cuda().unsqueeze(0)  # 1 x 6 x 6
 
-  with grad_op():
-    contact_normal = contact_normal / torch.norm(contact_normal, dim=-1, keepdim=True)
+energy = compute_energy(obj_code, z, contact_point_indices, sd_weight=100)
+old_energy = energy.clone()
+grad = torch.autograd.grad(energy.sum(), z)[0]
+grad_ema = EMA(args.mu)
+grad_ema.apply(grad)
 
-    hand_normal = hand_model.get_surface_normals(verts=hand_verts)
-    hand_normal = torch.stack([hand_normal[torch.arange(z.shape[0]), contact_point_indices[:,i], :] for i in range(3)], dim=1)
-    hand_normal = hand_normal / torch.norm(hand_normal, dim=-1, keepdim=True)    
+for _iter in range(10000):
+    step_size = 0.1
+    temperature = 1e-3
+    noise = torch.normal(mean=0, std=1, size=z.shape, device='cuda').float() * step_size
+    new_z = z - 0.5 * grad * mask[:,_iter%15,:] / grad_ema.average.unsqueeze(0) * step_size * step_size + noise
+    new_energy = compute_energy(obj_code, new_z, contact_point_indices, sd_weight=100)
+    new_grad = torch.autograd.grad(new_energy.sum(), new_z)[0]
+    alpha = torch.rand(args.batch_size, device='cuda').float()
+    accept = (alpha < torch.exp((energy - new_energy) / temperature)).long()
+    z = z * (1-accept.unsqueeze(1)) + new_z * accept.unsqueeze(1)
+    energy = energy * (1-accept) + new_energy * accept
+    grad = grad * (1-accept.unsqueeze(1)) + new_grad * accept.unsqueeze(1)
+    grad_ema.apply(grad)
+    if _iter % 100 == 0:
+        print(_iter, (energy-old_energy).mean().detach().cpu().numpy(), accept.float().mean().detach().cpu().numpy())
 
-    normal_alignment = ((hand_normal * contact_normal).sum(-1) + 1).sum(-1)
-    linear_independence, force_closure, surface_distance = fc_loss_model.fc_loss(contact_point, contact_normal, obj_code)
-    penetration = penetration_model.get_penetration_from_verts(obj_code, hand_verts)  # B x V
-    z_norm = torch.norm(z[:,-6:], dim=-1)
-    loss = linear_independence + force_closure + surface_distance.sum(1) + penetration.sum(1) + z_norm * 0.001 + normal_alignment
-    if verbose:
-      return linear_independence, force_closure, surface_distance.sum(1), penetration.sum(1), z_norm * 0.001, normal_alignment
-    else:
-      return loss
-
-# def visualize(obj_code, contact_point_indices, z, i_item):
-#   obj_mesh = get_obj_mesh_by_code(obj_code[i_item])
-#   hand_verts = hand_model.get_vertices(z).detach().float().cuda()
-#   contact_point = torch.stack([hand_verts[torch.arange(hand_verts.shape[0]), contact_point_indices[:,i],:] for i in range(3)], dim=1)
-#   hand_verts = hand_verts.detach().cpu().numpy()
-#   contact_point = contact_point.detach().cpu().numpy()
-#   fig = plotly.tools.make_subplots(1, 1, specs=[[{'type': 'surface'}]])
-#   fig.append_trace(go.Mesh3d(
-#     x=obj_mesh.vertices[:,0], y=obj_mesh.vertices[:,1], z=obj_mesh.vertices[:,2], i=obj_mesh.faces[:,0], j=obj_mesh.faces[:,1], k=obj_mesh.faces[:,2], 
-#     color='lightblue', opacity=0.5
-#   ), 1, 1)
-#   fig.append_trace(go.Mesh3d(
-#     x=hand_verts[i_item,:,0], y=hand_verts[i_item,:,1], z=hand_verts[i_item,:,2], i=hand_model.faces[:,0], j=hand_model.faces[:,1], k=hand_model.faces[:,2], 
-#     color='lightpink', opacity=0.5
-#   ), 1, 1)
-#   fig.append_trace(go.Scatter3d(
-#     x=contact_point[i_item,:,0], y=contact_point[i_item,:,1], z=contact_point[i_item,:,2], mode='markers'
-#   ), 1, 1)
-#   fig.show()
-
-# visualize(obj_code, contact_point_indices, z, i_item)
-
-# import matplotlib.pyplot as plt
-energy = compute_energy(obj_code, z, contact_point_indices, verbose=True)
-energy_entries = ['linear_independence', 'force_closure', 'surface_distance', 'penetration', 'z_norm', 'normal_alignment']
-# for i in range(6):
-#   # plt.subplot(3,3,i+1)
-#   # plt.hist(energy[i].detach().cpu().numpy())
-#   # plt.title(energy_entries[i])
-#   print(energy_entries[i], energy[i][i_item].detach().cpu().numpy())
-
-# plt.show()
-
-# print('hand_loss', hand_loss[i_item].detach().cpu().numpy())
-# print('linear_independence', linear_independence[i_item].detach().cpu().numpy())
-# print('force_closure', force_closure[i_item].detach().cpu().numpy())
-# print('surface_distance', surface_distance[i_item].detach().cpu().numpy())
-# print('penetration', penetration[i_item].detach().cpu().numpy())
-# print('z_norm', z_norm[i_item].detach().cpu().numpy())
-# print('normal_alignment', normal_alignment[i_item].detach().cpu().numpy())
-
-# plt.show()
-
-# energy_history = [[] for _ in range(6)]
-
-# plt.ion()
-
-# obj_code = obj_code[[i_item, i_item-1]]
-# z = z[[i_item, i_item]]
-# contact_point_indices = contact_point_indices[[i_item, i_item-1]]
-
-# print(z[0,-6:])
-
-ema = EMA(0.05)
-# ax = [plt.subplot(2,3,i+1) for i in range(6)]
-
-for _iter in range(5000):
-  for idx in [np.arange(0,128), np.arange(128,256)]:
-    linear_independence, force_closure, surface_distance, penetration, z_norm, normal_alignment = compute_energy(obj_code[idx], z[idx], contact_point_indices[idx], verbose=True)
-    grad = torch.autograd.grad((force_closure + surface_distance + penetration).sum(), z[idx], retain_graph=True)[0]
-    z[idx,-6:] = z[idx,-6:] - grad[:,-6:] * 2e-2
-    # ema.apply(grad)
-    # grad = grad / ema.average
-    # z = z - grad * 1e-5
-    # energy_history[0].append(linear_independence[0].detach().cpu().numpy())
-    # energy_history[1].append(force_closure[0].detach().cpu().numpy())
-    # energy_history[2].append(surface_distance[0].detach().cpu().numpy())
-    # energy_history[3].append(penetration[0].detach().cpu().numpy())
-    # energy_history[4].append(z_norm[0].detach().cpu().numpy())
-    # energy_history[5].append(normal_alignment[0].detach().cpu().numpy())
-    # if _iter % 100 == 0 or _iter == 4999:
-    #   for i in range(6):
-    #     ax[i].cla()
-    #     ax[i].plot(energy_history[i])
-    #     ax[i].set_title(energy_entries[i])
-    #   plt.pause(1e-5)
-    print(_iter, linear_independence.detach().cpu().numpy().mean(), force_closure.detach().cpu().numpy().mean(), surface_distance.detach().cpu().numpy().mean(), penetration.detach().cpu().numpy().mean(), z_norm.detach().cpu().numpy().mean(), normal_alignment.detach().cpu().numpy().mean())
-
-# print(energy[i_item].detach().cpu().numpy())
-energy = compute_energy(obj_code, z, contact_point_indices, verbose=True)
-energy_entries = ['linear_independence', 'force_closure', 'surface_distance', 'penetration', 'z_norm', 'normal_alignment']
-
-for i in range(6):
-  # plt.subplot(3,3,i+1)
-  # plt.hist(energy[i].detach().cpu().numpy())
-  # plt.title(energy_entries[i])
-  print(energy_entries[i], energy[i][0].detach().cpu().numpy())
-
-pickle.dump([obj_code, z, contact_point_indices, energy], open('logs/%s/optimized.pkl'%(name), 'wb'))
-# visualize(obj_code, contact_point_indices, z, 0)
-# input()
+pickle.dump([obj_code, z, contact_point_indices], open(os.path.join(log_dir, 'optim.pkl'), 'wb'))
